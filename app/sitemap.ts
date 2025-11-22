7ad61c31b41e41bea635529feeb5d5493e21fa0b
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2025-11-22 08:36:45 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next"
 import { carsData } from "@/lib/car-data"
 
+export const dynamic = "force-static"
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://kotsepedia.vercel.app"
 
