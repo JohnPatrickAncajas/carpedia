be022247fa22b621774ca9f3fb5ddb381903e8a7
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2025-11-23 20:46:32 +0800

    feat: new icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -13,9 +13,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   }
